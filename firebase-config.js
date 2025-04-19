1fdffa7946bfeb300702d2f547749ac39e84e057
Sayoon Yang <ficonomist@gmail.com>
2025-04-19 22:20:56 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
-
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCi7ZEBHbFxCEteOjvj-5aC7OhtadHwBYo",
   authDomain: "wallofmessages-94b11.firebaseapp.com",
   projectId: "wallofmessages-94b11",
-  storageBucket: "wallofmessages-94b11.appspot.com",
+  storageBucket: "wallofmessages-94b11.firebasestorage.app",
   messagingSenderId: "8844212877",
   appId: "1:8844212877:web:d1069b7c1dbc576af8c0c4",
   measurementId: "G-835E8RTBRG"
@@ -16,9 +15,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Save message
-await addDoc(collection(db, "messages"), {
-  content: message,
-  timestamp: new Date()
-});
+export { db };
+
 
