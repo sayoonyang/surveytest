365ea756f71876137d110406a92731ade3d39470
Sayoon Yang <ficonomist@gmail.com>
2025-04-19 22:00:51 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,19 @@
 
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCi7ZEBHbFxCEteOjvj-5aC7OhtadHwBYo",
+  authDomain: "wallofmessages-94b11.firebaseapp.com",
+  projectId: "wallofmessages-94b11",
+  storageBucket: "wallofmessages-94b11.appspot.com",
+  messagingSenderId: "8844212877",
+  appId: "1:8844212877:web:d1069b7c1dbc576af8c0c4",
+  measurementId: "G-835E8RTBRG"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
